fcca62183da1477269c555ddc251daf62c092126
Umar Farooq <umar_farooq52@outlook.com>
2023-11-10 17:19:52 +0500

    change username

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 // applying fade transition on .hidden elements
-const fadeElements = document.querySelectorAll('.hidden');
+const fadeElements = document.querySelectorAll(".hidden");
 
 const section_main = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
+  entries.forEach((entry) => {
+    console.log(entry);
 
-        if (entry.isIntersecting) entry.target.classList.add('fade');
-        else entry.target.classList.remove('fade')
-    });
+    if (entry.isIntersecting) entry.target.classList.add("fade");
+    else entry.target.classList.remove("fade");
+  });
 });
 
 fadeElements.forEach((el) => section_main.observe(el));
 
 // applying left slide transition section_1
 
-const slideElements = document.querySelectorAll('.slide');
+const slideElements = document.querySelectorAll(".slide");
 
 const section_1 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
+  entries.forEach((entry) => {
+    console.log(entry);
 
-        if (entry.isIntersecting) entry.target.classList.add('slide_on');
-        else entry.target.classList.remove('slide_on')
-    });
+    if (entry.isIntersecting) entry.target.classList.add("slide_on");
+    else entry.target.classList.remove("slide_on");
+  });
 });
 
 slideElements.forEach((el) => section_1.observe(el));
@@ -30,19 +30,20 @@ slideElements.forEach((el) => section_1.observe(el));
 // navbar hover Effect
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
-document.querySelector(".hoverEffect").onmouseover = event => {
-    let iterations = 0;
+document.querySelector(".hoverEffect").onmouseover = (event) => {
+  let iterations = 0;
 
-    const interval = setInterval(() => {
-        event.target.innerText = event.target.innerText.split("").map((letter, index) => {
-            if (index < iterations)  return event.target.dataset.value[index];
+  const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iterations) return event.target.dataset.value[index];
 
-            return letters[Math.floor(Math.random() * 26)]
-        }).join("");
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
 
-        if (iterations >= 20) clearInterval(interval);
-        iterations += 1 / 2;
-
-    }, 30)
-
-}
+    if (iterations >= 25) clearInterval(interval);
+    iterations += 1;
+  }, 30);
+};
